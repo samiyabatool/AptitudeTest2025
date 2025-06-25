bf6787c4b877c2116bf04819ca957ca1769a8d3f
samiyabatool <samiya.batool@stud.uni-bamberg.de>
2025-06-26 03:58:55 +0500

    Update types

---
+++ frontend/src/types/car.ts
@@ -0,0 +1,18 @@
+export interface Car {
+   _id: string;
+  Brand: string;
+  Model: string;
+  AccelSec: number | null;
+  TopSpeed_KmH: number | null;
+  Range_Km: number | null;
+  Efficiency_WhKm: number | null;
+  FastCharge_KmH: number | null;
+  RapidCharge: string | null;
+  PowerTrain: string | null;
+  PlugType: string | null;
+  BodyStyle: string | null;
+  Segment: string | null;
+  Seats: number | null;
+  PriceEuro: number | null;
+  Date: string | null;
+}
